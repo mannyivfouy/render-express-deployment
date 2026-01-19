4af98540b5482e4ee8fb61dc22e1454a3b7ab7d8
Mann Yivfouy <mannyivfouy@gmail.com>
2026-01-19 19:41:36 +0700

    Fix PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = express.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("API");
